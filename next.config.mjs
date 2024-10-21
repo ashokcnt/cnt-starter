d8715bd3666b0b75fe763f8f4db1ff07be77a3c1
ashokcnt <ashok.reddy@codeandtheory.com>
2024-10-21 16:38:05 +0530

    new

--- next.config.mjs
+++ next.config.mjs
@@ -8,16 +8,6 @@ jiti("./src/env");
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "images.ctfassets.net",
-        port: "",
-        pathname: "/**",
-      },
-    ],
-  },
 };
 
 export default nextConfig;
